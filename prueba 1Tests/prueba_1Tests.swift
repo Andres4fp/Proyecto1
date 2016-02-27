45a0c1341d5284c37ee03729aaa54b0bfa730928
Andrés Floriano <Andres@macbook-pro-de-andres-floriano.local>
2016-02-27 16:12:40 -0600

    Initial Commit

---
+++ prueba 1Tests/prueba_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  prueba_1Tests.swift
+//  prueba 1Tests
+//
+//  Created by Andrés Floriano on 27/02/16.
+//  Copyright © 2016 Andrés Floriano. All rights reserved.
+//
+
+import XCTest
+@testable import prueba_1
+
+class prueba_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
